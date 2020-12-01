c11e87fcc00409370bbfe27de45597e687e21cfc
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-01 08:54:32 -0500

    Update force-user.py

--- force-user.py
+++ force-user.py
@@ -1,13 +1,9 @@
-#By SxNade
-#https://github.com/SxNade
-#CONTRIBUTE
-
 import os
+import sys
 
 list = ['john','ftp','Odis','Yagi','Edwardo','Herlihy','Leandro','Norberg','Maximo','Duford', 'admin', 'root', 'bob', 'jugnu', 'manglu']
-#List of users to check for the smtp service...
 
+IP = sys.argv[1]
 
-#a loop to run the final script for each user present in the list
 for user in list:
-	os.system(f'python3 smtp_user_brtfrce.py {user}')
+	os.system(f'python3 smtp_user_brtfrce.py {user} {IP}')
