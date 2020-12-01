7b893c51f9deff3549a334c790af6e594b35ac4b
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-01 08:54:27 -0500

    Update smtp_user_brtfrce.py

--- smtp_user_brtfrce.py
+++ smtp_user_brtfrce.py
@@ -1,30 +1,31 @@
-#By SxNade
-#https://github.com/SxNade
-#CONTRIBUTE
-
 import socket
 import sys
 from termcolor import colored
 
-if len(sys.argv) != 2:
-	print(colored("usage script.py <username>", "red"))
+if len(sys.argv) != 3:
+	print(colored("usage script.py <username> <target-ip>", "red"))
 	sys.exit(0)
 
 
 s = socket.socket()
-connect = s.connect(('192.168.0.105', 25))
+connect = s.connect((sys.argv[2], 25))
 
 data = s.recv(1024).decode()
 
 command = 'VRFY ' + sys.argv[1] + '\n'
 
-s.send(command.encode())
-result = s.recv(1024)
-fin_result = result.decode()
-valid = '550'
-if valid.encode() in result:
-	print(colored(sys.argv[1] + " is not a valid user..", "red"))
-else:
-	print(colored(sys.argv[1] + " is a valid user..", 'blue', attrs=['bold']))
-s.close()
+def brtmgc():
+	s.send(command.encode())
+	result = s.recv(1024)
+	fin_result = result.decode()
+	valid = '550'
+	if valid.encode() in result:
+		print(colored(sys.argv[1] + " is not a valid user..", "red"))
+	else:
+		print(colored(sys.argv[1] + " is a valid user..", 'blue', attrs=['bold']))
+	s.close()
 
+def main():
+	brtmgc()
+
+main()
