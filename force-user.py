46f90df02d579286a2c7446e75f2b3e475964ea9
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-01 09:24:22 -0500

    Update force-user.py

--- force-user.py
+++ force-user.py
@@ -5,10 +5,14 @@
 
 import os
 import sys
+#import the required libraries
 
+#list of usernames to test
 list = ['john','ftp','Odis','Yagi','Edwardo','Herlihy','Leandro','Norberg','Maximo','Duford', 'admin', 'root', 'bob', 'jugnu', 'manglu']
 
+#defining the IP variable to receiving the IP address of the server
 IP = sys.argv[1]
 
+#A for loop to run the main script for each username in the defined list
 for user in list:
 	os.system(f'python3 smtp_user_brtfrce.py {user} {IP}')
