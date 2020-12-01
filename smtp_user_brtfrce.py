04e746ccdc0711fc920d70cae093fcaba9d7a0d7
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-01 09:21:32 -0500

    Update smtp_user_brtfrce.py

--- smtp_user_brtfrce.py
+++ smtp_user_brtfrce.py
@@ -6,19 +6,22 @@
 import socket
 import sys
 from termcolor import colored
+#importing the required libraries
 
+#conditional statement for grabing [sys] arguments for username and ip address
 if len(sys.argv) != 3:
 	print(colored("usage script.py <username> <target-ip>", "red"))
 	sys.exit(0)
 
-
+#initiating socket to form network connections
 s = socket.socket()
 connect = s.connect((sys.argv[2], 25))
-
 data = s.recv(1024).decode()
 
+#command variable containing data to send to the server
 command = 'VRFY ' + sys.argv[1] + '\n'
 
+#The function containing the final code to interact with the server and also receive the data back from the SMTP server
 def brtmgc():
 	s.send(command.encode())
 	result = s.recv(1024)
@@ -30,7 +33,9 @@ def brtmgc():
 		print(colored(sys.argv[1] + " is a valid user..", 'blue', attrs=['bold']))
 	s.close()
 
+#defining main function containing the brtmgc fucntion
 def main():
 	brtmgc()
 
+#final main function to run the whole function
 main()
