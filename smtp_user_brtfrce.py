b7eecc2139ea44b9393a066a398921f5d4b05de6
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-14 00:40:07 -0500

    Update smtp_user_brtfrce.py

--- smtp_user_brtfrce.py
+++ smtp_user_brtfrce.py
@@ -1,4 +1,3 @@
- 
 #By SxNade
 #https://github.com/SxNade
 #CONTRIBUTE
@@ -6,36 +5,49 @@
 import socket
 import sys
 from termcolor import colored
+import os
 #importing the required libraries
+
+target_ip = sys.argv[1]
+user_file = sys.argv[2]
 
 #conditional statement for grabing [sys] arguments for username and ip address
 if len(sys.argv) != 3:
 	print(colored("usage script.py <username> <target-ip>", "red"))
 	sys.exit(0)
 
-#initiating socket to form network connections
-s = socket.socket()
-connect = s.connect((sys.argv[2], 25))
-data = s.recv(1024).decode()
 
-#command variable containing data to send to the server
-command = 'VRFY ' + sys.argv[1] + '\n'
+if os.path.exists(user_file) == False:
+	print(colored('[!]File Not Found....exiting now..!', 'red', attrs=['bold']))
+	sys.exit(0)
 
-#The function containing the final code to interact with the server and also receive the data back from the SMTP server
-def brtmgc():
-	s.send(command.encode())
-	result = s.recv(1024)
-	fin_result = result.decode()
-	valid = '550'
-	if valid.encode() in result:
-		print(colored(sys.argv[1] + " is not a valid user..", "red"))
-	else:
-		print(colored(sys.argv[1] + " is a valid user..", 'blue', attrs=['bold']))
-	s.close()
+with open(f'{user_file}', 'r') as file:
+	for line in file.readlines():
+		user = line.strip()
+		#command variable containing data to send to the server
+		command = 'VRFY ' + user + '\n'
+		try:
+			s = socket.socket()
+			connect = s.connect((target_ip, 25))
+			data = s.recv(1024).decode()
+			s.send(command.encode())
+			result = s.recv(1024)
+			fin_result = result.decode()
+			valid = '550'
+			if valid.encode() in result:
+				print(colored(f"\n[!]{user} is not a valid user..", "red", attrs=['bold']))
+			else:
+				print(colored(f"\n\n[+] {user} is a valid user..\n\n", 'green', attrs=['bold']))
+			s.close()
+		except KeyboardInterrupt:
+			ask = input("\n\nDo you want to stop Bruteforce: ")
+			if ask == "Y":
+				sys.exit(0)
+			elif ask == "N":
+				pass
+			else:
+				print(colored("\nUnexpected input"))
+				sys.exit(0)
 
-#defining main function containing the brtmgc fucntion
-def main():
-	brtmgc()
 
-#final main function to run the whole function
-main()
+
