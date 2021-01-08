38374cdc67e4422e6e56697c25b354f8695980ed
SxNade <73271976+SxNade@users.noreply.github.com>
2021-01-08 12:17:38 -0500

    Update smtp_user_brtfrce.py

--- smtp_user_brtfrce.py
+++ smtp_user_brtfrce.py
@@ -44,22 +44,27 @@ print(golo)
 time.sleep(1)
 print("\n (v2.0) starting...")
 
+#check for correct number of arguments
 if len(sys.argv) != 3:
 	print(colored("\nusage script.py <target-ip> <username-file--or--path> \n", 'white', attrs=['reverse', 'blink']))
 	sys.exit(0)
 
 target_ip = sys.argv[1]
 user_file = sys.argv[2]
+#grabbing the necessary arguments
 
+#check if the given file exists on the system
 if os.path.exists(user_file) == False:
 	print(colored('[!]File Not Found....exiting now..!', 'red', attrs=['bold']))
 	sys.exit(0)
 
+#reading passwords from the file!
 with open(f'{user_file}', 'r') as file:
 	for line in file.readlines():
 		user = line.strip()
 		#command variable containing data to send to the server
 		command = 'VRFY ' + user + '\n'
+		#using socket to connect to SMTP on the Target
 		try:
 			s = socket.socket()
 			connect = s.connect((target_ip, 25))
@@ -84,4 +89,4 @@ with open(f'{user_file}', 'r') as file:
 				sys.exit(0)
 
 
-
+#BYE
